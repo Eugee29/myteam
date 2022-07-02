a7e30c27dfb020056bdfa27cc0ad2012b475d31b
Uri Gruda <uri2920@gmail.com>
2022-07-02 13:30:36 +0300

    made the 'build & manage' section responsive

--- src/cmps/call-to-action.jsx
+++ src/cmps/call-to-action.jsx
@@ -1,3 +1,11 @@
+import { ReactComponent as BgSvg } from '../assets/svg/bg-pattern-home-6-about-5.svg'
+
 export const CallToAction = () => {
-  return <section>call to action</section>
+  return (
+    <section className="call-to-action">
+      <h1 className="title">Ready to get started?</h1>
+      <a className="contact">contact us</a>
+      <BgSvg className="bg-svg" />
+    </section>
+  )
 }
